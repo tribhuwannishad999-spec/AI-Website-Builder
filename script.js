63443d4477e7e0d654abc147b079d3726c65eae9
tribhuwannishad999-spec <tribhuwannishad999@gmail.com>
2026-06-20 13:32:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function generateWebsite(){
+
+let prompt=document.getElementById("prompt").value;
+
+document.getElementById("code").innerText=
+"AI Response Will Come Here...\n\nPrompt:\n"+prompt;
+
+}
